976f5cd1cbde33ad09ea03b6afb6342b03d44a93
OK_Diamond <ok.thekirks@gmail.com>
2024-04-30 20:21:20 +0100

    -

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const SUPABASE_URL = 'https://iwjygprrmbkssvcpbdpz.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3anlncHJybWJrc3N2Y3BiZHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyODI4NjAsImV4cCI6MjAyODg1ODg2MH0.xYNt7yBxi6os26Uzi-_pk_YPR4bC7A3VsIzt9njt5Jk'
-var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+var _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 async function loadData() {
-    const { data, error } = await supabase
+    const { data, error } = await _supabase
             .from('posts')
             .select()
 

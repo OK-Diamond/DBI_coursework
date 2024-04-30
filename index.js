d1e18cb4b490230e55d433b271fb549e199bb15a
OK_Diamond <ok.thekirks@gmail.com>
2024-04-30 20:06:28 +0100

    -

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-import { createClient } from '@supabase/supabase-js'
+const SUPABASE_URL = 'https://iwjygprrmbkssvcpbdpz.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3anlncHJybWJrc3N2Y3BiZHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyODI4NjAsImV4cCI6MjAyODg1ODg2MH0.xYNt7yBxi6os26Uzi-_pk_YPR4bC7A3VsIzt9njt5Jk'
+var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
+async function loadData() {
+    const { data, error } = await supabase
+            .from('posts')
+            .select()
+
+    if(!error) {
+        const parent = document.getElementById('holder')
+
+        let contents = ''
+        data.forEach(function(item){
+            contents += `<div> ${item.title} - ${item.tag}</div>` 
+        })
+
+        parent.insertAdjacentHTML('beforeend', contents)
+    }
+    
+}
+loadData()

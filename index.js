881be6cf52968a59dc8b539f11d39dfad3482eb0
OK_Diamond <ok.thekirks@gmail.com>
2024-05-12 20:08:41 +0100

    Porgress on grid layout

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const SUPABASE_URL = 'https://iwjygprrmbkssvcpbdpz.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3anlncHJybWJrc3N2Y3BiZHB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyODI4NjAsImV4cCI6MjAyODg1ODg2MH0.xYNt7yBxi6os26Uzi-_pk_YPR4bC7A3VsIzt9njt5Jk'
 var _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
-function addToContents(data) {
-    var div
-    for (var i = 0; i < data.length; i++) {
-        div = document.createElement('div')
-        div.innerHTML = data[i].tag
-        document.getElementById('holder').appendChild(div)
-    }
+
+
+function submit(p1, p2) {
+    // Test function
+    alert("p1: "+p1+" p2: "+p2);
 }
+
 
 async function loadData() {
     const { data, error } = await _supabase
